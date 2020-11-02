42515a2dc415146f54db3cd3c34e56835b6dfad3
iptablecommandcode <iptablecommandcode@gmail.com>
2020-11-02 15:09:42 +0900

    count

--- rock_siser_papger.py
+++ rock_siser_papger.py
@@ -1,3 +1,10 @@
+"""
+가위바위보 추가 제작
+가위바위보 총 플레이 계산, 승리,패배,무승부 계산 추가
+개발 프로그램 : VSCode
+작성자 : 박치원
+이메일 : iptablecommandcode@gmail.com
+"""
 import random
 import sys
 
@@ -5,6 +12,10 @@ Re = 1
 com = ['가위', '바위', '보']
     # 가위바위 카운트 전역 처리
 counttot = 0
+countwin = 0
+countlose = 0
+countdraw = 0
+
 while(Re == 1):
     rec = 0
     mine = 0
@@ -24,23 +35,30 @@ while(Re == 1):
     print('너는',mine,'를 내었고, 컴퓨터는',comp, '를 내었네요')
     if(mine == comp):
         print('따라서 당신은 비겼습니다.')
+        countdraw += 1
     else:
         if(mine == '가위'):
             if(comp == '바위'):
                 print('따라서 당신은 졌습니다.')
+                countlose += 1
             if(comp == '보'):
                 print('따라서 당신은 이겼습니다.')
+                countwin += 1
         else:
             if(mine == '바위'):
                 if(comp == '보'):
                     print('따라서 당신은 졌습니다.')
+                    countlose += 1
                 if(comp == '가위'):
                     print('따라서 당신은 이겼습니다.')
+                    countwin += 1
             else:
                 if(comp == '가위'):
                     print('따라서 당신은 졌습니다.')
+                    countlose += 1
                 if(comp == '바위'):
                     print('따라서 당신은 이겼습니다.')
+                    countwin += 1
 
         while rec not in ['예','아니오']:
             rec = input('다시 시작 할 것입니까? (예, 아니오)')
@@ -48,5 +66,5 @@ while(Re == 1):
                 re = 1
             else:
                 print('프로그램을 종료합니다.')
-                print('당신은 총',counttot,'번 하였습니다.')
+                print('당신은 총',counttot,'번 하였고 승리는 총',countwin,'패배는 총',countlose,'무승부는 총',countdraw)
                 sys.exit()
